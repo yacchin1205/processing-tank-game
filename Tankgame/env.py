a136084708c65dcc0bcecacb58818dca13dd804f
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2020-06-10 21:23:39 +0900

    Fix sensor

--- Tankgame/env.py
+++ Tankgame/env.py
@@ -1,3 +1,27 @@
+def is_observable(world, heroes, enemy):
+    for hero in heroes:
+        dx = enemy.x - hero.x
+        dy = enemy.y - hero.y
+        observable = True
+        if abs(dx) > abs(dy):
+            a = dy / dx
+            for x in range(int(min(enemy.x, hero.x)), int(max(enemy.x, hero.x))):
+                y = a * (x - hero.x) + hero.y
+                if world.grid[min(int(y / world.cell_size), len(world.grid) - 1)][min(int(x / world.cell_size), len(world.grid[0]) - 1)] == 1:
+                    observable = False
+                    break
+        else:
+            a = dx / dy
+            for y in range(int(min(enemy.y, hero.y)), int(max(enemy.y, hero.y))):
+                x = a * (y - hero.x) + hero.y
+                if world.grid[min(int(y / world.cell_size), len(world.grid) - 1)][min(int(x / world.cell_size), len(world.grid[0]) - 1)] == 1:
+                    observable = False
+                    break
+        if observable:
+            return True
+    return False
+
+
 class WrappedWorld:
     def __init__(self, world):
         self.grid_width = world.grid_width
@@ -5,7 +29,8 @@ class WrappedWorld:
         self.grid = [row.copy() for row in world.grid]
 
 class WrappedTank:
-    def __init__(self, tank, is_hero):
+    def __init__(self, id, tank, is_hero):
+        self.id = id
         self.x = tank.x
         self.y = tank.y
         self.dead = tank.dead
@@ -21,6 +46,7 @@ class WrappedBullet:
 class Environment:
     def __init__(self, world, heroes, enemies, bullets):
         self.world = WrappedWorld(world)
-        self.heroes = [WrappedTank(h, True) for h in heroes]
-        self.enemies = [WrappedTank(e, False) for e in enemies]
+        self.heroes = [WrappedTank(i, h, True) for i, h in enumerate(heroes)]
+        self.enemies = [WrappedTank(i, e, False) for i, e in enumerate(enemies)
+                        if is_observable(world, heroes, e)]
         self.bullets = [WrappedBullet(b) for b in bullets]
