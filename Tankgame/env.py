40e9f6c8ccb16d5c5f65c86c41aada90fbc450f5
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2020-06-24 20:25:45 +0900

    Fix notebooks

--- Tankgame/env.py
+++ Tankgame/env.py
@@ -1,40 +1,5 @@
 def is_observable(world, heroes, enemy):
-    for i, hero in enumerate(heroes):
-        hero_label = world.grid_label[min(int(hero.real_y / world.cell_size), len(world.grid) - 1)][min(int(hero.real_x / world.cell_size), len(world.grid[0]) - 1)]
-        enemy_label = world.grid_label[min(int(enemy.real_y / world.cell_size), len(world.grid) - 1)][min(int(enemy.real_x / world.cell_size), len(world.grid[0]) - 1)]
-        if hero_label == enemy_label:
-            return True
-    return False
-
-def flood_fill(grid, empty_label, start_label):
-    next_label = start_label
-    while True:
-        empty = None
-        for y in range(len(grid)):
-            for x in range(len(grid[0])):
-                if grid[y][x] == empty_label:
-                    empty = (x, y)
-                    break
-            if empty is not None:
-                break
-        if empty is None:
-            # finished
-            break
-        x, y = empty
-        _flood_fill(grid, x, y, empty_label, next_label)
-        next_label += 1
-    return grid
-
-def _flood_fill(grid, x, y, empty_label, label):
-    grid[y][x] = label
-    targets = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-    for x_, y_ in targets:
-        if x_ < 0 or y_ < 0 or x_ >= len(grid[0]) or y_ >= len(grid):
-            continue
-        if grid[y_][x_] != empty_label:
-            continue
-        grid[y_][x_] = label
-        _flood_fill(grid, x_, y_, empty_label, label)
+    return True
 
 
 class WrappedWorld:
@@ -43,7 +8,6 @@ class WrappedWorld:
         self.grid_width = world.grid_width
         self.grid_height = world.grid_height
         self.grid = [row.copy() for row in world.grid]
-        self.grid_label = flood_fill([row.copy() for row in world.grid], 0, 2)
 
 class WrappedTank:
     def __init__(self, id, tank, is_hero):
